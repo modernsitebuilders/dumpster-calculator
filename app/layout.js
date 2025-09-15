bad933ee2995c30b2ef4ca48ecb040b6d1173313
David Miles <dave@modernsitebuilders.com>
2025-09-15 11:50:48 -0400

    favicon

--- app/layout.js
+++ app/layout.js
@@ -42,6 +42,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         {/* JSON-LD Structured Data for Rich Snippets */}
         <script
           type="application/ld+json"
