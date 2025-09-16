9e76e29378052c585df658151cd07abfd165c1bb
David Miles <dave@modernsitebuilders.com>
2025-09-16 11:07:55 -0400

    header update

--- app/components/Header.js
+++ app/components/Header.js
@@ -11,8 +11,9 @@ export default function Header() {
           </Link>
           <div className="flex space-x-6">
             <Link href="/" className="text-gray-600 hover:text-blue-600">Calculator</Link>
-            <Link href="/about" className="text-gray-600 hover:text-blue-600">About</Link>
             <Link href="/blog" className="text-gray-600 hover:text-blue-600">Blog</Link>
+            <Link href="/local" className="text-gray-600 hover:text-blue-600">Local Guides</Link>
+            <Link href="/about" className="text-gray-600 hover:text-blue-600">About</Link>
             <Link href="/contact" className="text-gray-600 hover:text-blue-600">Contact</Link>
           </div>
         </nav>
