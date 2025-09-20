5c6556181c01207065076aab3f967b543304d5ac
David Miles <dave@modernsitebuilders.com>
2025-09-20 12:42:01 -0400

    undo cal

--- app/local/guides/california/page.js
+++ app/local/guides/california/page.js
@@ -163,8 +163,7 @@ export default function CaliforniaStatePage() {
                 </div>
 
                 {/* City Info */}
-                <div className="p-8 flex flex-col h-full">
-                  
+<div className="p-8 flex flex-col">                  
                   {/* Pricing */}
                   <div className="flex items-center justify-between mb-6">
                     <div className="flex items-center text-green-600">
